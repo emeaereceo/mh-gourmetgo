d373e41713565c936a0cf02cf95d30e9257700e6
emeaereceo <entelequia.dev@gmail.com>
2025-12-30 20:58:02 -0300

    Añadiendo interactividad, api buscador de recetas, api buscar detalle de recetas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,91 @@
-// // * 1. Crear funcion para la busqueda
-// //   TODO : Revisar api para conocer los parametros que se le pueden pasar
-// // * 2. Vincular funcion al boton del formulario, al escuchar el evento
-// // * 3. Funcion retorna una lista de objetos, se recorre la lista para crear cards con la info
-// //  [{}, {}, {}]
+// * 1. Crear funcion para la busqueda
+//   TODO : Revisar api para conocer los parametros que se le pueden pasar
+// * 2. Vincular funcion al boton del formulario, al escuchar el evento
+// * 3. Funcion retorna una lista de objetos, se recorre la lista para crear cards con la info
 
-// async function buscarRecetas(ingrediente) {
-//   const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`;
+// Busqueda de receta por ingrediente
+async function buscarRecetas(ingrediente) {
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`;
 
-//   try {
-//     const respuesta = await fetch(url);
-//     const datos = await respuesta.json();
+  try {
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    // console.log("Recetas encontradas:", datos.meals);
+    return datos.meals;
+  } catch (error) {
+    console.error("Error al buscar la receta:", error);
+  }
+}
 
-//     console.log("Recetas encontradas:", datos.meals);
-//     return datos.meals;
-//   } catch (error) {
-//     console.error("Error al buscar la receta:", error);
-//   }
-// }
+// Detalle de la receta por ID
+async function detalleReceta(id) {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  try {
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    // console.log(datos);
+    return datos.meals[0];
+  } catch (error) {
+    console.error("Error al buscar la receta:", error);
+  }
+}
 
-// // buscarRecetas("beef");
+// buscarRecetas("beef");
+const searchBtn = document.getElementById("btn-buscar");
 
-// document.getElementById("btn-buscar").addEventListener("click", async () => {
-//   const ingrediente = document.getElementById("input-ingrediente").value.trim();
-//   const contenedor = document.getElementById("resultado");
+searchBtn.addEventListener("click", async () => {
+  const ingrediente = document.getElementById("input-ingrediente").value.trim();
+  const contenedor = document.getElementById("resultado");
 
-//   contenedor.innerHTML = "<p>Cargando...</p>";
+  contenedor.innerHTML = `
+  <div class="w-100 d-flex justify-content-center">
+    <div class="spinner-border text-success" role="status">
+      <span class="visually-hidden">Buscando</span>
+    </div>
+  </div>`;
 
-//   const recetas = await buscarRecetas(ingrediente);
+  const recetas = await buscarRecetas(ingrediente);
 
-//   if (!recetas) {
-//     contenedor.innerHTML = "<p>No se encontraron recetas.</p>";
-//     return;
-//   }
+  if (!recetas) {
+    contenedor.innerHTML = `<p>No se encontraron recetas con ingrediente ${ingrediente}.</p>`;
+    return;
+  }
 
-//   contenedor.innerHTML = recetas
-//     .map(
-//       (meal) => `
-//         <div class="col-12 col-md-6 col-lg-4">
-//          <div class="recipe-card card h-100 shadow-sm border-0">
-//             <img
-//               src="${meal.strMealThumb}"
-//               class="card-img-top"
-//               alt="${meal.strMeal}"
-//             />
-//             <div class="card-body">
-//               <h5 class="card-title fw-semibold">${meal.strMeal}</h5>
-//               <a href="#" class="btn btn-outline-primary rounded-pill"
-//                 >Ver receta</a
-//               >
-//             </div>
-//           </div>
-//         </div>
-//       `
-//     )
-//     .join("");
-// });
+  contenedor.innerHTML = recetas
+    .map(
+      (meal) => `
+        <div class="col-12 col-md-6 col-lg-4">
+         <div class="recipe-card card h-100 shadow-sm border-0">
+            <img
+              src="${meal.strMealThumb}"
+              class="card-img-top"
+              alt="${meal.strMeal}"
+            />
+            <div class="card-body">
+              <h5 class="card-title fw-semibold">${meal.strMeal}</h5>
+              <a href="#" class="btn btn-outline-success rounded-pill ver-receta" data-id=${meal.idMeal}
+                >Ver receta</a
+              >
+            </div>
+          </div>
+        </div>
+      `
+    )
+    .join("");
+});
 
-// Js Minimo para activar el modal con la imagen y el titulo
-document.addEventListener("click", (e) => {
-  if (!e.target.classList.contains("ver-receta")) return;
+document.addEventListener("click", async (evento) => {
+  if (!evento.target.classList.contains("ver-receta")) return;
 
-  const boton = e.target;
+  const id = evento.target.dataset.id;
 
-  // Obtener datos del botón
-  const titulo = boton.dataset.title;
-  const imagen = boton.dataset.img;
+  const receta = await detalleReceta(id);
 
   // Pasarlos al modal
-  document.getElementById("modalTitulo").textContent = titulo;
-  document.getElementById("modalImagen").src = imagen;
-  document.getElementById("modalImagen").alt = titulo;
+  document.getElementById("modalTitulo").textContent = receta.strMeal;
+  document.getElementById("modalImagen").src = receta.strMealThumb;
+  document.getElementById("modalImagen").alt = receta.strMeal;
+  document.getElementById("modalDetalle").textContent = receta.strInstructions;
 
   // Mostrar modal
   const modal = new bootstrap.Modal(document.getElementById("modalReceta"));
